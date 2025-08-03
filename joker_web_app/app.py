c93274d62ca2b5ed598d3f849b99477f7f3ce5a8
GioMeli <136624687+GioMeli@users.noreply.github.com>
2025-08-03 15:09:09 +0300

    Update app.py

--- joker_web_app/app.py
+++ joker_web_app/app.py
@@ -105,8 +105,8 @@ def generate_prediction(df, num_predictions=5, seed_source="", actual_draw=None)
 
     recent_draws = df[["Num1", "Num2", "Num3", "Num4", "Num5"]].tail(5).values
     kmeans, cluster_centers = cluster_draws(df)
-    closest_cluster = find_closest_cluster(kmeans, recent_draws)
-    closest_cluster_numbers = list(set(closest_cluster.astype(int)))
+    _, cluster_centers_raw = find_closest_cluster(kmeans, recent_draws)
+    closest_cluster_numbers = list(set(cluster_centers_raw.astype(int)))
 
     for _ in range(num_predictions):
         attempt = 0
